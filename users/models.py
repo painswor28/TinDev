fe1c0c94a88aeb142fd700d19ab68491ab9635f1
painswor28 <93224337+painswor28@users.noreply.github.com>
2022-11-18 16:52:40 -0500

    new app

--- users/models.py
+++ users/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.models import User
 
-# Create your models here.
-
 
 class Candidate(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
